336377fa436cf231b821e921b530a11331b5de57
velkovskis <svelkovski67@gmail.com>
2026-09-23 23:00:46 +0200

    feat: implement and wire the interpreter

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -9,7 +9,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
+
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,6 +31,7 @@ public class Lox {
 
         // Indicate an  error in the exit code.
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -51,6 +55,8 @@ public class Lox {
 
         // Stop if there was a syntax error.
         if (hadError) return;
+
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -67,5 +73,10 @@ public class Lox {
     static void error(Token token, String message) {
         if (token.type == TokenType.EOF) report(token.line, " at end", message);
         else report(token.line, " at '" + token.lexeme + "'", message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.out.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
